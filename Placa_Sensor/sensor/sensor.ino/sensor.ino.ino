24aff00599c888eb5512c6b3c4434938699e90e1
Dedin16 <pedroneto293@gmail.com>
2017-06-22 11:29:44 -0300

    Implementadas: ping, num_dados e clear

--- Placa_Sensor/sensor/sensor.ino/sensor.ino.ino
+++ Placa_Sensor/sensor/sensor.ino/sensor.ino.ino
@@ -9,8 +9,8 @@ RF24 myRadio (7, 8);    //(CE,CSN)
 byte addresses[][4] = {"ch1","ch2"};
 
 char incomingByte;
-char mensagem[100];
-int i=0, tempo=0;
+char mensagem[100]="";
+int i=0, tempo=0,size_msg=0;
 char fim=0, temperatura=0;
 int tempo_aquisicao=10;
 
@@ -24,7 +24,7 @@ void setup()
   Serial.println("Funcionando");
 
   //Incializa Timer
-  Timer1.initialize(500000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
+  Timer1.initialize(500000); // set a timer of length 500000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
   Timer1.attachInterrupt( timerIsr ); // attach the service routine here
   delay(1000);
 
@@ -108,10 +108,6 @@ byte read_dado(int posicao){
 void timerIsr()
 {   
     tempo++;
-    //int sensorValue = analogRead(A0);    
-    //float voltage = sensorValue*(5.0/1023.0);
-    //float T=((voltage-0.6)*100);
-    
 
 }
 
@@ -125,16 +121,43 @@ void grava_temperatura()
   
 }
 
+void envia_RF(char *msg, int tam){
+  char  enter='\n';
+  int j = 0;
+  
+  myRadio.stopListening();      //Modo envio
+              
+  for(j=0;j<tam-1;j++){
+     if(!myRadio.write(&msg[j], sizeof(char)))   //Envia caracter para RF
+     {
+          Serial.println("Erro na Transmisão");
+     }
+  }
+  if(!myRadio.write(&enter, sizeof(char)))   //Envia caracter para RF
+  {
+          Serial.println("Erro na Transmisão");
+  }
+
+  myRadio.startListening();   //Modo escuta
+
+}
+
 void loop(){
     
     if(tempo>tempo_aquisicao){
         grava_temperatura();
         int tamanho=dados_mem();
+
+        //Read dados
         byte valor_lido_hi =read_dado(tamanho-1);
         byte valor_lido_lo =read_dado(tamanho);
+
+        //Conversao
         int eepromValue = (valor_lido_hi<<8) + valor_lido_lo;
         float voltage = eepromValue*(5.0/1023.0);
         float T=((voltage-0.6)*100);
+
+        /*
         Serial.println("A última temperatura gravada foi:");
         Serial.print(T);
         Serial.println("ºC");
@@ -143,17 +166,12 @@ void loop(){
         Serial.println(valor_lido_lo, HEX);
         Serial.print("DAdos na memória:  ");
         Serial.println(tamanho);
-
-        char  enter='\n';
+        */
+        
         /*
-        unsigned char lido_lo=valor_lido_lo;
-        unsigned char lido_hi=valor_lido_hi;
+        //Envia um dado para RF
         
         char  enter='\n';
-
-        Serial.println(lido_hi,HEX);
-        Serial.println(lido_lo,HEX);
-        */
         
         myRadio.stopListening();      //Modo envio
     
@@ -173,6 +191,7 @@ void loop(){
               }
         
         myRadio.startListening();   //Modo escuta
+        */
         
         tempo=0;
         temperatura=1;
@@ -197,11 +216,6 @@ void loop(){
           myRadio.startListening();   //Modo escuta
   }
 
-  if(temperatura)
-  {
-    temperatura=0;
-  }
-
 
   //Recepcao
   if (myRadio.available())       //Ha dados vindos do RF
@@ -211,26 +225,74 @@ void loop(){
         myRadio.read( &incomingByte, sizeof(incomingByte) );        //Le caracter do RF
         if(incomingByte!='\n')
         {
-            Serial.println("VIVO");
-            mensagem[i]= incomingByte;
-            i++;
+            mensagem[size_msg]= incomingByte;
+            size_msg++;
         }
         else
         {
-            mensagem[i]='\0';                                       // Caracter de fim de linha
-            i=0;
+            mensagem[size_msg]='\0';                                       // Caracter de fim de linha
             fim=1;
         }
       
     }
 
   }
-
+  
   if(fim)   //Trata a mensagem recebida
   {
-      Serial.print("Pessoa:");
-      Serial.println(mensagem);
-      fim=0;
+      
+      if(strcmp(mensagem,"ping")==0){
+        char  enter='\n';
+        char enviar[]= "pong";
+        Serial.println("pong");
+
+        delay(100);
+        envia_RF(enviar,sizeof(enviar));
+        
+        }
+
+     if(strcmp(mensagem,"num_dados")==0){
+        char num_data[]="100";
+        int  number= dados_mem()/2;
+        sprintf(num_data,"%d",number);
+        delay(100);
+        envia_RF(num_data,sizeof(num_data));
+     }
+
+     if(strcmp(mensagem,"clear")==0){
+         clear_mem();
+     }
+
+     if(strcmp(mensagem,"download")==0){
+          Serial.println("download");
+     }
+
+     if(strcmp(mensagem,"measure")==0){
+          Serial.println("measure");
+     }
+     
+     if(strcmp(mensagem,"play")==0){
+          Serial.println("play");
+     }
+
+     if(strcmp(mensagem,"stop")==0){
+          Serial.println("stop");
+     }
+
+     if(strcmp(mensagem,"freeze")==0){
+          Serial.println("freeze");
+     }
+
+     if(strcmp(mensagem,"get_n")==0){
+          Serial.println("get_n");
+     }
+
+
+
+     for(i=0;i<size_msg;i++)mensagem[i]='\0';   //Limpa string de mensagem
+     
+     size_msg=0;
+     fim=0;
   }
   }
 
